littleGnAl/mirror_platform_frameworks_native

installd: Simplify Dalvik cache path creation
Installd computes Dalvik cache path for a dex file by concatenating
'/classes.dex' to the APKs absolute path and then replacing all '/'
characters with '@'. OTA preopt does the same, only in reverse order,
i.e by concatenating '@classes.dex' to altered absolute path.

This patch unifies the two approaches so as to keep only one string
constant.

Change-Id: I69bb6bca831f45c873a0eb8580cf8d4b011f3b09

cmds/installd/installd.cpp
@@ -151,12 +151,11 @@ bool create_cache_path(char path[PKG_PATH_MAX],
         return false;
     }
 
-    sprintf(path,"%s%s/%s/%s%s",
+    sprintf(path,"%s%s/%s/%s",
             android_data_dir.path,
             DALVIK_CACHE,
             instruction_set,
-            src + 1, /* skip the leading / */
-            DALVIK_CACHE_POSTFIX);
+            src + 1 /* skip the leading / */);
 
     char* tmp =
             path +
@@ -170,6 +169,8 @@ bool create_cache_path(char path[PKG_PATH_MAX],
             *tmp = '@';
         }
     }
+
+    strcat(tmp, DALVIK_CACHE_POSTFIX);
 
     return true;
 }

DiscoveryTeam/platform_frameworks_native

cmds/installd/installd.cpp
@@ -151,12 +151,11 @@ bool create_cache_path(char path[PKG_PATH_MAX],
         return false;
     }
 
-    sprintf(path,"%s%s/%s/%s%s",
+    sprintf(path,"%s%s/%s/%s",
             android_data_dir.path,
             DALVIK_CACHE,
             instruction_set,
-            src + 1, /* skip the leading / */
-            DALVIK_CACHE_POSTFIX);
+            src + 1 /* skip the leading / */);
 
     char* tmp =
             path +
@@ -171,6 +170,7 @@ bool create_cache_path(char path[PKG_PATH_MAX],
         }
     }
 
+    strcat(path, DALVIK_CACHE_POSTFIX);
     return true;
 }
 